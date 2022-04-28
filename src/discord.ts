dfcac52bbc7ec0c84e32658940d38024c5da8554
RaphaelDk <raphael.dakich@gmail.com>
2022-04-28 17:23:11 +0200

    Unused discord intents removed

--- src/discord.ts
+++ src/discord.ts
@@ -15,8 +15,6 @@ export class Discord {
 			intents: [
 				Intents.FLAGS.GUILDS,
 				Intents.FLAGS.GUILD_MESSAGES,
-				Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-				Intents.FLAGS.GUILD_VOICE_STATES,
 				Intents.FLAGS.GUILD_MEMBERS,
 			],
 			// classes: [
